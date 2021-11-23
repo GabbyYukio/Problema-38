1dd475b6301bd1e1d815431f4ffe7b6e3d7134a9
GabbyYukio <gabbyyukio@gmail.com>
2021-11-23 08:30:16 +0200

    Add project files.

---
+++ FundamenteleProgramarii38/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//38.Scrieţi o functie care să calculeze factorialul unui număr întreg n
+
+namespace FundamenteleProgramarii38
+{
+    class Program
+    {
+        static ulong factorialul(ulong n)//returneaza long pentru ca numerele pot sa fie foarte mari
+        {
+            ulong e=1;
+            while(n!=1)
+            {
+                e = e * n;
+                n--;
+            }
+            return e;
+
+        }
+        static void Main(string[] args)
+        {
+            //INPUT
+            ulong n; //factorialul nu poate sa fie negativ si este intotdeauna pozitiv
+            Console.WriteLine("Dati valoare lui n: ");
+            n=ulong.Parse(Console.ReadLine());
+
+            Console.WriteLine("Factorialul lui {0} este {1}.", n, factorialul(n));
+            Console.ReadLine();
+        }
+    }
+}
